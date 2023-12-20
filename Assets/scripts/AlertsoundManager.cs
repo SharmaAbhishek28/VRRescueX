3c1803e2623b15a44a828cfae0ca3830ae20a21b
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-12-20 16:07:50 +0530

    Heptic to sound mamanger

--- Assets/scripts/AlertsoundManager.cs
+++ Assets/scripts/AlertsoundManager.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
+using UnityEngine.XR;
 
-public class AlertsoundManager : MonoBehaviour
+public class AlertSoundManager : MonoBehaviour
 {
+    public XRControllerHapticFeedback hapticFeedbackScript;
     private AudioSource audioSource;
-    public bool loop = true;
+    public bool loop=true;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        if (!loop)
-        audioSource.loop = false;
+        if (loop)
+        {
+            audioSource.loop = true;
+        }
+        else
+        {
+            audioSource.loop = false;
+        }
         // Component is initially inactive, so we don't play the alert sound here.
     }
 
     private void OnEnable()
     {
-        gameObject.SetActive(true);
         // Start playing the alert sound when the component becomes active.
         PlayAlertSound();
+
+        // Activate haptic feedback
+        if (hapticFeedbackScript != null)
+        {
+            hapticFeedbackScript.ToggleLeftHapticFeedback(true);
+            hapticFeedbackScript.ToggleRightHapticFeedback(true);
+        }
 
         // Invoke a method to pause the sound and deactivate the component after 3 seconds.
         Invoke("PauseSoundAndDeactivate", 3f);
@@ -42,5 +56,12 @@ public class AlertsoundManager : MonoBehaviour
 
         // Deactivate the component.
         gameObject.SetActive(false);
+
+        // Deactivate haptic feedback
+        if (hapticFeedbackScript != null)
+        {
+            hapticFeedbackScript.ToggleLeftHapticFeedback(false);
+            hapticFeedbackScript.ToggleRightHapticFeedback(false);
+        }
     }
 }
